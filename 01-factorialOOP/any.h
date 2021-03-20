b1c8ed644eaf561ec5358957b9ae60d28d390494
nlchar <contact.me@nlchar.rocks>
2021-03-20 17:30:59 +0300

    Translated any.h

--- 01-factorialOOP/any.h
+++ 01-factorialOOP/any.h
@@ -3,23 +3,30 @@
 
 #include <iostream>
 
-//------------------------------------------------------------------------------
 // Структура, формирующая родительский класс,
 // подключаемый ко всем дочерним классам
+//--------------------------------------------
+// The structure that forms the parent class
+// that is to be used as the abstraction for any of its child classes (EO objects)
 struct EO_any {
-    // Виртуальный метод, реализующий вычисление внутри объекта
+    // Виртуальный метод, реализующий вычисление (датаризацию) объекта
+    //--------------------------------------------
+    // The virtual evaluation method that performs the dataization operation of the object
     virtual EO_any* eval() = 0;
-    // Виртуальный метод, реализующий вычисление с результатом в заданном объекте
-    //virtual bool eval(EO_any& reciver) = 0;
 
-    // получение имени объекта (пока для тестирования)
+    // получение имени (иначе: типа) объекта (пока для тестирования)
+    //--------------------------------------------
+    // retrieves the name of the object (in other words, the type of the object) 
+    // abstracted by the EO_any (for testing purposes)
     virtual const char* getTypeName() = 0;
+    
     // вывод тестовой информации об объекте
+    //-------------------------------------
+    // prints the debug information about the object
     virtual void testOut() {
         std::cout << "Name is: " << getTypeName()
                   << "\n";
     }
-    
 };
 
 // Функция - оболочка для обезличивания типа создаваемого объекта. 
